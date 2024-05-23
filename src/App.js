e652273e9529a3dcf47533f0baef7dad231560fc
Tataata13 <emili.edel.ten@gmail.com>
2024-05-23 13:16:24 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Clock } from './Clock';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Local time:</h1>
+      <Clock/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Clock.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+export class Clock extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            time: new Date().toLocaleTimeString()
+        }
+    }
+    
+    componentDidMount(){
+        this.timerID = setInterval(() => 
+        this.tick(),1000)
+    }
+    
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+
+    tick() {
+        this.setState({
+            time: new Date().toLocaleTimeString()
+        })
+    }
+
+    render() {
+        return (
+            
+                <p className="Time">{this.state.time}</p>
+        
+        )
+    }
+}
